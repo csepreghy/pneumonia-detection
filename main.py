6ae36c7e26ad8809710a9ba88d65027bfdbb8ad0
Andrew <andris788@gmail.com>
2020-11-19 16:34:23 +0100

    added to main.py from kaggle

--- main.py
+++ main.py
@@ -8,10 +8,69 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
+import cv2
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
+
+for dirname, _, filenames in os.walk('data'):
     for filename in filenames:
-        print(os.path.join(dirname, filename))
+        if '.DS_Store' not in filename:
+            print(os.path.join(dirname, filename))
 
 # Any results you write to the current directory are saved as output.
 
+labels = ['PNEUMONIA', 'NORMAL']
+img_size = 150
+
+def get_training_data(data_dir):
+    data = [] 
+    for label in labels: 
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+
+            except Exception as e:
+                print(e)
+
+    return np.array(data)
+
+
+train = get_training_data('data/train')
+test = get_training_data('data/test')
+val = get_training_data('data/val')
+
+l = []
+for i in train:
+    if(i[1] == 0):
+        l.append("Pneumonia")
+    else:
+        l.append("Normal")
+
+sns.set_style('darkgrid')
+sns.countplot(l)
+plt.show()
+
+plt.figure(figsize = (5,5))
+plt.imshow(train[0][0], cmap='gray')
+plt.title(labels[train[0][1]])
+plt.show()
+
+plt.figure(figsize = (5,5))
+plt.imshow(train[-1][0], cmap='gray')
+plt.title(labels[train[-1][1]])
+plt.show()
